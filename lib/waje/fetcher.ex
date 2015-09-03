c9c75c359110eb459672c5f96dcda7fb20f39d47
Jasper St. Pierre <jstpierre@mecheye.net>
2015-09-03 16:51:47 -0700

    fetcher: Remove server
    There's no use for this anymore

--- lib/waje/fetcher.ex
+++ lib/waje/fetcher.ex
@@ -1,8 +1,5 @@
 defmodule Waje.Fetcher do
-  use GenServer
-
   def start_link() do
-    GenServer.start_link(__MODULE__, :ok, [name: :fetcher])
     GenEvent.start_link([name: :fetcher_events])
   end
 
